a3e3654db8016a554a09e1a16be7850170763836
tfshnike <tfshnike@gmail.com>
2013-10-05 13:57:25 +0800

    Add badge Component

--- badge.js
+++ badge.js
@@ -2,7 +2,7 @@ var EL = Ext.Element;
 //Ext.get('elid').addBadge(5, 'br-br', null, true)
 EL.addMethods({
 	addBadge: function(num, position, offset, animate, monitorScroll, callback) {
-		var me = this, badge = null, shouldUpdateNum = false;
+		var me = this, badge = null, shouldUpdateNum = false, originalNum = me.badge.num;
 		me.badge = me.badge || me.createChild({tag: 'span', html: num});
 		badge = me.badge;
 		shouldUpdateNum = Ext.isDefined(num) && (badge.num != num);
@@ -14,33 +14,47 @@ EL.addMethods({
 			monitorScroll: Ext.isDefined(monitorScroll) ? num : badge.monitorScroll,
 			callback: Ext.isDefined(callback) ? callback : badge.callback
 		});
-		if (shouldUpdateNum) {
-			badge.update(badge.num);
-		}
+		var updateFn = function() {
+			if (shouldUpdateNum) {
+				badge.update(badge.num);
+			}
+		};
+		updateFn();
 		badge.anchorTo(me, badge.position, badge.offset, false, badge.monitorScroll, null);
-		if (!shouldUpdateNum || !badge.doAnimate) {
+		if (!shouldUpdateNum) {
 			return badge;
 		}
-		var animateCallBack = badge.animate.createDelegate(badge, [
-    		{
-        		opacity: {to: 1, from: 0.3},
-        		top  : {by: 10, unit: 'px'}
-    		},
-    		0.35,      // animation duration
-    		badge.callback,      // callback
-    		'easein', // easing method
-    		'run'      // animation type ('run','color','motion','scroll')
-		]);
-		badge.animate(
-    		{
-        		opacity: {to: 0.3, from: 1},
-        		top  : {by: -10, unit: 'px'}
-    		},
-    		0.35,      // animation duration
-    		animateCallBack,      // callback
-    		'easeOut', // easing method
-    		'run'      // animation type ('run','color','motion','scroll')
-		);
+
+		if (shouldUpdateNum) {
+			badge.update(originalNum);
+		}
+		if (badge.doAnimate) {
+			var animateCallBack = badge.animate.createDelegate(badge, [
+	    		{
+	        		opacity: {to: 1, from: 0.3},
+	        		top  : {by: 10, unit: 'px'}
+	    		},
+	    		0.35,      // animation duration
+	    		badge.callback,      // callback
+	    		'easein', // easing method
+	    		'run'      // animation type ('run','color','motion','scroll')
+			]);
+			var animUpdateFn = updateFn.createSequence(animateCallBack, badge);
+		
+			badge.animate(
+	    		{
+	        		opacity: {to: 0.3, from: 1},
+	        		top  : {by: -10, unit: 'px'}
+	    		},
+	    		0.35,      // animation duration
+	    		animUpdateFn,      // callback
+	    		'easeOut', // easing method
+	    		'run'      // animation type ('run','color','motion','scroll')
+			);
+		} else {
+			updateFn();
+		}
+		
 		return badge; 
 	},
 	anchorBadgeTo: function(position, offset, animate, monitorScroll, callback) {
---
+++ badgeComponent.js
@@ -0,0 +1,85 @@
+/**
+ * @class Ext.ux.Badge
+ * @extends Ext.BoxComponent
+ * @xtype ux_badge
+ */
+Ext.ux.Badge = Ext.extend(Ext.BoxComponent, {
+   /**
+    * @cfg {String} baseCls
+    * The base CSS class to apply to the badgeelement (defaults to 'ux-badge')
+    */
+    baseCls: 'ux-badge',
+
+    /**
+    * @cfg {Boolean} animate
+    * True to animate the badge during transitions (defaults to true)
+    */
+    animate: true,
+
+    /**
+    * @cfg {Number} animateDistance
+    * How far to move the badge (defaults to 10)
+    */
+    animateDistance: 10,
+    
+    /**
+    * @cfg {Number} animateDelayTime
+    * delay to update the badge value (defaults to 350)
+    */
+    animateDelayTime: 350,
+
+    // private
+    onRender: function(ct, position) {
+    	var me = this;
+        var tpl = new Ext.Template(
+            '<span class="{cls}">',
+            '</span>'
+        );
+
+        me.el = position ? tpl.insertBefore(position, {cls: this.baseCls}, true)
+            : tpl.append(ct, {cls: this.baseCls}, true);
+                
+        if(me.id){
+            me.el.dom.id = me.id;
+        }
+    },
+    
+    // private
+    afterRender: function() {
+    	var me = this;
+        Ext.ux.Badge.superclass.afterRender.call(me);
+        if(me.value){
+            me.updateText(me.value, true);
+        }
+    },
+
+    /**
+     * Updates the badge value.
+     * @param {String} value (optional) The string to display (defaults to 0)
+     * @param {Boolean} force (optional) force to update value
+     * @return {Ext.ux.Badge} this
+     */
+    updateText: function(value, force){
+    	var me = this, badge = me.el,
+    	shouldUpdatevalue = force === true || (Ext.isDefined(value) && (me.value != value));
+        me.value = value || 0;
+ 
+        if(me.rendered && !me.isDestroyed && shouldUpdatevalue) {
+			if (me.animate) {
+				badge.addClass('ux-badge-animate-up');
+				badge.move('up', me.animateDistance);
+			}
+			badge.update.defer(me.animateDelayTime || 350, badge , [me.value]);
+			if (me.animate) {
+				badge.removeClass.defer(me.animateDelayTime || 350, badge, ['ux-badge-animate-up']);
+				badge.move.defer(me.animateDelayTime || 350, badge, ['down', me.animateDistance]);
+			}
+        }
+        return me;
+    },
+    anchorTo: function(position, offset, animate, monitorScroll, callback) {
+    	var me = this, badge = me.el;
+    	badge.anchorTo(me.container, position, offset, animate, monitorScroll, callback);
+    }
+});
+Ext.reg('ux_badge', Ext.ux.Badge);
